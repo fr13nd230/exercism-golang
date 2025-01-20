6b5526ab6487293baa27a9ef4dc9a4488ae658a5
Abderrahmane KADDOUR <darkvally4@gmail.com>
2025-01-20 18:56:23 +0100

    Commenting ...

--- twofer/twofer.go
+++ twofer/twofer.go
@@ -5,6 +5,11 @@ import (
 	"strings"
 )
 
+/*
+* Problem's Name: Two Fer
+* Difficulty: Easy
+ */
+
 func ShareWith(name string) string {
 	if len(strings.TrimSpace(name)) == 0 {
 		return "One for you, one for me."
